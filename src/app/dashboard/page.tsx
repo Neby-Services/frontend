6bcf051138f76608e2826eee6f08c80578d6c438
JayexDesigns <joaquintouron@gmail.com>
2024-03-24 13:08:42 +0100

    task #94: added dashboard page and service card component

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+	return (
+		<main>
+			<section className="max-w-[1800px] min-[1800px]:mx-auto box-content py-8 px-4 md:px-16 grid grid-cols-1 gap-6 xl:grid-cols-2"></section>
+		</main>
+	);
+}
