4ef19848dd2df897eaeb483216318efc8c653d1c
Sk0404 <santiagogab2019@gmail.com>
2024-04-09 12:31:35 +0200

    task #112: added show password button to register form

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -16,6 +16,8 @@ export default function Register() {
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
 	const [confirmPassword, setConfirmPassword] = useState("");
+	const [passwordVisible, setPasswordVisible] = useState(false);
+	const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
 	const [userType, setUserType] = useState("neighbor");
 	const [communityCode, setCommunityCode] = useState("");
 	const [communityName, setCommunityName] = useState("");
@@ -84,11 +86,51 @@ export default function Register() {
 							</label>
 							<label className="flex flex-col text-sm sm:text-lg font-semibold gap-2 mb-6">
 								Password
-								<input onChange={e => setPassword(e.target.value)} value={password} className="border-2 rounded-lg text-base p-1.5" type="password" />
+								<div className="relative">
+									<button onClick={() => setPasswordVisible(!passwordVisible)}>
+										<svg className="absolute top-1/2 -translate-y-1/2 right-4 size-6" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24">
+											{passwordVisible ? (
+												<>
+													<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+													<path d="M10.585 10.587a2 2 0 0 0 2.829 2.828" />
+													<path d="M16.681 16.673a8.717 8.717 0 0 1 -4.681 1.327c-3.6 0 -6.6 -2 -9 -6c1.272 -2.12 2.712 -3.678 4.32 -4.674m2.86 -1.146a9.055 9.055 0 0 1 1.82 -.18c3.6 0 6.6 2 9 6c-.666 1.11 -1.379 2.067 -2.138 2.87" />
+													<path d="M3 3l18 18" />
+												</>
+											) : (
+												<>
+													<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+													<path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+													<path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
+												</>
+											)}
+										</svg>
+									</button>
+									<input onChange={e => setPassword(e.target.value)} value={password} className="border-2 rounded-lg text-base p-1.5 w-full pr-12" type={passwordVisible ? "text" : "password"} />
+								</div>
 							</label>
 							<label className="flex flex-col text-sm sm:text-lg font-semibold gap-2 mb-6">
 								Confirm Password
-								<input onChange={e => setConfirmPassword(e.target.value)} value={confirmPassword} className="border-2 rounded-lg text-base p-1.5" type="password" />
+								<div className="relative">
+									<button onClick={() => setConfirmPasswordVisible(!confirmPasswordVisible)}>
+										<svg className="absolute top-1/2 -translate-y-1/2 right-4 size-6" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" viewBox="0 0 24 24">
+											{confirmPasswordVisible ? (
+												<>
+													<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+													<path d="M10.585 10.587a2 2 0 0 0 2.829 2.828" />
+													<path d="M16.681 16.673a8.717 8.717 0 0 1 -4.681 1.327c-3.6 0 -6.6 -2 -9 -6c1.272 -2.12 2.712 -3.678 4.32 -4.674m2.86 -1.146a9.055 9.055 0 0 1 1.82 -.18c3.6 0 6.6 2 9 6c-.666 1.11 -1.379 2.067 -2.138 2.87" />
+													<path d="M3 3l18 18" />
+												</>
+											) : (
+												<>
+													<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+													<path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+													<path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
+												</>
+											)}
+										</svg>
+									</button>
+									<input onChange={e => setConfirmPassword(e.target.value)} value={confirmPassword} className="border-2 rounded-lg text-base p-1.5 w-full pr-12" type={confirmPasswordVisible ? "text" : "password"} />
+								</div>
 							</label>
 							<div className="flex flex-col text-sm sm:text-lg font-semibold gap-2 mb-6">
 								<label>User Type</label>
