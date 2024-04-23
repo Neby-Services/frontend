88d181a9d1f2189879ff21b31ecc9371aaa58097
JayexDesigns <joaquintouron@gmail.com>
2024-04-24 00:29:18 +0200

    task #115: logo animation and responsive landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,9 @@ import Link from "next/link";
 export default function Home() {
 	return (
 		<main className="min-h-svh grid place-content-center">
-			<section className="max-w-[90%] mx-auto h-svh flex flex-row justify-center items-center gap-12">
+			<section className="max-w-[1400px] w-screen px-10 sm:px-20 lg:px-40 mx-auto h-svh flex flex-col-reverse md:flex-row justify-center md:justify-between items-center md:gap-12">
 				<div>
-					<h1 className="text-5xl font-bold mb-6">NEBY</h1>
+					<h1 className="text-5xl font-bold mb-6 max-md:text-center">NEBY</h1>
 					<h2 className="text-2xl font-medium mb-10">
 						The solution for an organized <span className="highlight">community</span>
 					</h2>
@@ -16,7 +16,7 @@ export default function Home() {
 						<li>Gamified experience with an achievement system</li>
 						<li>Manage your own integrated economy</li>
 					</ol>
-					<span className="flex flex-row items-center gap-4">
+					<span className="flex flex-row max-md:justify-center items-center gap-4">
 						<Link href="/register">
 							<Button variant="default" size="default" className="font-semibold rounded-full">
 								Register
@@ -29,17 +29,13 @@ export default function Home() {
 						</Link>
 					</span>
 				</div>
-				<div className="relative size-72 min-w-72">
+				<div
+					className={`
+					w-[150px] max-w-[150px] h-[200px] max-h-[200px]
+					sm:w-[200px] sm:max-w-[200px] sm:h-[250px] sm:max-h-[250px]
+					md:w-[300px] md:max-w-[300px] md:h-[400px] md:max-h-[400px]
+				`}>
 					<LogoAnimation />
-					{/* <svg className="z-50 w-full text-foreground absolute top-[40%] left-[50%] -translate-x-1/2 -translate-y-1/2" viewBox="0 0 1000 1000">
-						<path d="M431.80459 211.50004a67.730267 67.730267 0 0 0-30.36274 17.52491L75.000001 555.46693l95.775349 95.77536 278.55418-278.55432 101.33464 101.33465 95.77536-95.77536-149.22232-149.22231a67.730267 67.730267 0 0 0-65.41262-17.52491Zm397.42006 137.25668L550.67044 627.31073 449.33583 525.96988l-95.77535 95.77536 149.22229 149.22868a67.730267 67.730267 0 0 0 95.77535 0L925 444.53207Z" fill="currentColor" stroke="none" strokeWidth="10" strokeLinecap="square" strokeLinejoin="round" strokeMiterlimit="1.5" strokeDasharray="none" strokeOpacity="1" />
-					</svg>
-					<svg className="z-40 w-full text-secondary absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2" viewBox="0 0 1000 1000">
-						<path d="M431.80459 211.50004a67.730267 67.730267 0 0 0-30.36274 17.52491L75.000001 555.46693l95.775349 95.77536 278.55418-278.55432 101.33464 101.33465 95.77536-95.77536-149.22232-149.22231a67.730267 67.730267 0 0 0-65.41262-17.52491Zm397.42006 137.25668L550.67044 627.31073 449.33583 525.96988l-95.77535 95.77536 149.22229 149.22868a67.730267 67.730267 0 0 0 95.77535 0L925 444.53207Z" fill="none" stroke="currentColor" strokeWidth="10" strokeLinecap="square" strokeLinejoin="round" strokeMiterlimit="1.5" strokeDasharray="none" strokeOpacity="1" />
-					</svg>
-					<svg className="z-30 w-full text-secondary absolute top-[60%] left-[50%] -translate-x-1/2 -translate-y-1/2" viewBox="0 0 1000 1000">
-						<path d="M431.80459 211.50004a67.730267 67.730267 0 0 0-30.36274 17.52491L75.000001 555.46693l95.775349 95.77536 278.55418-278.55432 101.33464 101.33465 95.77536-95.77536-149.22232-149.22231a67.730267 67.730267 0 0 0-65.41262-17.52491Zm397.42006 137.25668L550.67044 627.31073 449.33583 525.96988l-95.77535 95.77536 149.22229 149.22868a67.730267 67.730267 0 0 0 95.77535 0L925 444.53207Z" fill="none" stroke="currentColor" strokeWidth="10" strokeLinecap="square" strokeLinejoin="round" strokeMiterlimit="1.5" strokeDasharray="none" strokeOpacity="1" />
-					</svg> */}
 				</div>
 			</section>
 		</main>
