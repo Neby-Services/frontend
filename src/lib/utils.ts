2c4b8377ab3f474d10513929f13551880c58cbbe
David Baque <baquedavid0@gmail.com>
2024-05-31 12:07:30 +0200

    task #179: fix achievements bug

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
-import {clsx, type ClassValue} from "clsx";
-import {toast} from "sonner";
-import {twMerge} from "tailwind-merge";
+import { clsx, type ClassValue } from "clsx";
+import { toast } from "sonner";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
@@ -17,4 +17,17 @@ export function formToastError(message: string) {
 	return toast.error(message, {
 		position: "top-right"
 	});
+}
+
+export function capitalizeFirstLetter(word: string) {
+	if (word.length === 0) {
+		return word;
+	}
+	return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+export enum AchievementStatus {
+	IN_PROGRESS = "in_progress",
+	CLAIMED = "claimed",
+	COMPLETED = "completed"
 }
